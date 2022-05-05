fec3a5d659198f78839b72632d87e930531c53ed
flowfordan <flowfordan@gmail.com>
2022-05-05 20:10:23 +0300

    styling

--- src/components/StoryItem/StoryItem.js
+++ src/components/StoryItem/StoryItem.js
@@ -3,7 +3,6 @@ import { APIService } from '../../services/apiService';
 import { useEffect, useState } from 'react';
 import { NavLink, Link } from 'react-router-dom';
 
-const apiService = new APIService();
 
 export const StoryItem = (props) => {
 
@@ -17,9 +16,12 @@ export const StoryItem = (props) => {
     return(
         <div className={styles.cardWrapper}>
             <span className={styles.title}>{title}</span>
+            <span className={styles.author}>
+                <a href={url}>{url}</a>
+            </span>
             <span className={styles.rate}>{rating}</span>
             <span className={styles.author}>{author}</span>
-            <span className={styles.author}>{url}</span>
+            
             <span className={styles.date}>{date}</span>
             <div>{`Comments (${commentsNum})`}</div>    
         </div>
