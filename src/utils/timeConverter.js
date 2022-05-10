5189240f87bd2c45028585b1096c88b0b85f6bcd
flowfordan <flowfordan@gmail.com>
2022-05-10 15:12:21 +0300

    time convert fix

--- src/utils/timeConverter.js
+++ src/utils/timeConverter.js
@@ -10,8 +10,8 @@ export const timeConverter = (unix) => {
   const month = months[a.getMonth()];
   const date = a.getDate();
   const hour = a.getHours();
-  const min = a.getMinutes();
-  const sec = a.getSeconds();
+  const min = a.getMinutes().toString().length === 1? `0${a.getMinutes()}` : a.getMinutes();
+  const sec = a.getSeconds().toString().length === 1? `0${a.getSeconds()}` : a.getSeconds();
   const time = `${date} ${month} ${year} ${hour}:${min}:${sec}`;
   return time;
 }
